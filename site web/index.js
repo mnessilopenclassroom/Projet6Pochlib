bc767c74df2ec228f24fe5ea9d419ea636aaf251
mnessilopenclassroom <73446554+mnessilopenclassroom@users.noreply.github.com>
2021-03-17 13:37:24 +0100

    initial commit

---
+++ site web/index.js
@@ -0,0 +1,16 @@
+ const btn = document.createElement('button');
+
+ btn.innerText = 'Ajouter un livre';
+
+var placerButton = document.querySelector('.h2').appendChild(btn);
+
+
+btn.style.width = '200px';
+btn.style.height = '40px';
+btn.style.fontSize = '1 em';
+btn.style.color = 'white';
+btn.style.backgroundColor = '#059773';
+
+
+
+
